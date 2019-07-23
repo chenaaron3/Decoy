37dc63829ae4e2601d9c6853ad82f63e22e8f663
Aaron Chen Laptop <chenaaron3@gmail.com>
2019-07-22 17:54:04 -0700

    Clone Detects Hits

--- Assets/Scripts/Player/Clone.cs
+++ Assets/Scripts/Player/Clone.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Clone : MonoBehaviour
 {
+    HashSet<GameObject> hitHistory;
 
     private const string SHADER_COLOR_NAME = "_Color";
     private Material material;
@@ -11,6 +12,7 @@ public class Clone : MonoBehaviour
 
     private void Start()
     {
+        hitHistory = new HashSet<GameObject>();
         sr = transform.Find("Graphics").GetComponent<SpriteRenderer>();
         // makes a new instance of the material for runtime changes
         material = sr.material;
@@ -19,6 +21,13 @@ public class Clone : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // If collided with enemy attacks, give player attacks
+        if(collision.CompareTag("EnemyDamager"))
+        {
+            if(!hitHistory.Contains(collision.gameObject))
+            {
+                hitHistory.Add(collision.gameObject);
+            }
+        }
     }
 
     public void Die(float duration)
